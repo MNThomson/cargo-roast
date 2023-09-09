02430c4767f1095efd464faa0a05a9f389ffe970
Max Thomson <git@mnt.dev>
2023-09-08 19:36:57 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main(){
+    println!("Oh, no! Looks like we stumbled upon a gold mine of errors in your code. Your code is so chaotic, it's like a tornado tore through it, leaving only a trail of confusion behind. And those errors? They're as rare as a unicorn sighting. Keep going and you'll soon become the Picasso of bug creations.")
+}
